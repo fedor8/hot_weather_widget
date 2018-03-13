8e1694378690aad1b28e0cebb195a8dae0b1672e
Fedor Dydykin <dydykin@softlab.ru>
2018-03-13 12:03:27 +0300

    i18n again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { CommonModule } from '@angular/common';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { ResortListComponent } from './components/resort-list/resort-list.component';
@@ -13,6 +14,7 @@ import { CategoryMenuComponent } from './components/category-menu/category-menu.
 import { ResortCardComponent } from './components/resort-card/resort-card.component';
 import {ResortsService} from './services/resorts.service';
 import { ResortFilterPipe } from './pipes/resort-filter.pipe';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -29,9 +31,22 @@ import { ResortFilterPipe } from './pipes/resort-filter.pipe';
     BrowserModule,
     CommonModule,
     FormsModule,
-    HttpModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [ResortsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
